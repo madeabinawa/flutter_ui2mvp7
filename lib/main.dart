aede7d847b63307aec0128edd18d93b58e56cfe7
I Made Dwija Abinawa <madeabinawa@gmail.com>
2020-11-22 11:39:15 +0800

    add provider login di halaman main dan login

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter_ui2mvp7/api/login_api.dart';
 import 'package:flutter_ui2mvp7/views/Login.dart';
 import 'package:flutter_ui2mvp7/shared_value.dart';
 
@@ -10,16 +12,23 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // primarySwatch: Colors.blue,
-        brightness: Brightness.light,
-        primaryColor: SharedColor.mainColor,
-        accentColor: SharedColor.secondColor,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => LoginProvider(),
+        ),
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          // primarySwatch: Colors.blue,
+          brightness: Brightness.light,
+          primaryColor: SharedColor.mainColor,
+          accentColor: SharedColor.secondColor,
+        ),
+        home: Login(),
+        debugShowCheckedModeBanner: false,
       ),
-      home: Login(),
-      debugShowCheckedModeBanner: false,
     );
   }
 }
--- lib/views/Login.dart
+++ lib/views/Login.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flushbar/flushbar.dart';
+import 'package:provider/provider.dart';
 import 'package:flutter_ui2mvp7/shared_value.dart';
-
+import 'package:flutter_ui2mvp7/api/login_api.dart';
 import 'package:flutter_ui2mvp7/views/pba/MainPba.dart';
 import 'package:flutter_ui2mvp7/views/pcu/MainPcu.dart';
 
@@ -12,14 +14,22 @@ class Login extends StatefulWidget {
 class _LoginState extends State<Login> {
   TextEditingController _email = TextEditingController();
   TextEditingController _password = TextEditingController();
+  bool _obscureText = true;
+
   @override
   Widget build(BuildContext context) {
+    //initial Login Provider
+    final loginProvider = Provider.of<LoginProvider>(context, listen: false);
+    //cek jika field username password kosong
     isControllerEmpty() {
       return _email.text != '' && _password.text != '';
     }
 
+    //get Screen size
     var screen = MediaQuery.of(context).size;
+    //node textField
     final node = FocusScope.of(context);
+
     return Scaffold(
       resizeToAvoidBottomPadding: false,
       body: Center(
@@ -60,10 +70,20 @@ class _LoginState extends State<Login> {
                     decoration: InputDecoration(
                         border: OutlineInputBorder(),
                         labelText: 'Password',
-                        prefixIcon: Icon(Icons.lock_outline)),
+                        prefixIcon: Icon(Icons.lock_outline),
+                        suffixIcon: IconButton(
+                          icon: _obscureText
+                              ? Icon(Icons.visibility_off)
+                              : Icon(Icons.visibility),
+                          onPressed: () {
+                            setState(() {
+                              _obscureText = !_obscureText;
+                            });
+                          },
+                        )),
                     controller: _password,
                     onEditingComplete: () => node.unfocus(),
-                    obscureText: true,
+                    obscureText: _obscureText,
                     style: TextStyle(),
                   ),
                   SizedBox(height: 20),
@@ -80,20 +100,39 @@ class _LoginState extends State<Login> {
                           shape: RoundedRectangleBorder(
                               borderRadius: BorderRadius.circular(10.0)),
                           onPressed: isControllerEmpty()
-                              ? () => Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                      builder: (context) => MainPba(),
-                                    ),
-                                  )
-                              : null,
-                          onLongPress: isControllerEmpty()
-                              ? () => Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                      builder: (context) => MainPcu(),
-                                    ),
-                                  )
+                              ? () async {
+                                  //Menerima result dari login api
+                                  LoginResult loginResult = await loginProvider
+                                      .login(_email.text, _password.text);
+                                  print(loginResult.roleId);
+
+                                  //jika role_id 4 maka navigasi ke halaman pcu
+                                  if (loginResult.roleId == 4) {
+                                    Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                        builder: (context) => MainPcu(),
+                                      ),
+                                    );
+                                  } else if (loginResult.roleId == 3) {
+                                    Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                        builder: (context) => MainPba(),
+                                      ),
+                                    );
+                                  }
+                                  //jika role_id tidak ditemukan tampilkan peringatan
+                                  else {
+                                    Flushbar(
+                                      duration: Duration(seconds: 4),
+                                      backgroundColor: Colors.red[800],
+                                      flushbarPosition: FlushbarPosition.BOTTOM,
+                                      message:
+                                          'Kesalahan pada email atau password',
+                                    ).show(context);
+                                  }
+                                }
                               : null,
                           color: isControllerEmpty()
                               ? SharedColor.mainColor
